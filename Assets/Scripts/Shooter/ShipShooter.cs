4a4ca04271d0a5d15d5d7bbe2c0e6353eb05cfaf
Amber Arnold <pinkangelrose12@gmail.com>
2023-05-31 16:24:40 -0700

    Added shooter
    things to do
    add rigibody to projectile
    fix shooter update

--- Assets/Scripts/Shooter/ShipShooter.cs
+++ Assets/Scripts/Shooter/ShipShooter.cs
@@ -2,17 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ShipShooter : MonoBehaviour
+
+public class ShipShooter : Shooter
 {
-    // Start is called before the first frame update
-    void Start()
+    public Transform firepointTransform;
+
+    public override void Shoot(GameObject shellPrefab, float fireforce, float damageDone, float lifespan)
+    {
+        //Spawn in bullet
+        GameObject newShell = Instantiate(shellPrefab, firepointTransform.position, firepointTransform.rotation) as GameObject;
+        //Set value for bullet
+        DamageOnHit damageOnHit = newShell.GetComponent<DamageOnHit>();
+        if (damageOnHit)
+        {
+            damageOnHit.damage = damageDone;
+            damageOnHit.owner = GetComponent<Pawn>();
+        }
+        Rigidbody rb = newShell.GetComponent<Rigidbody>();
+
+        if (rb)
+        {
+            rb.AddForce(firepointTransform.forward * fireforce);
+        }
+
+        Destroy(newShell, lifespan);
+
+    }
+
+    public override void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void Update()
     {
-        
+        throw new System.NotImplementedException();
     }
 }
--- Assets/Scripts/Shooter/Shooter.cs
+++ Assets/Scripts/Shooter/Shooter.cs
@@ -2,17 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Shooter : MonoBehaviour
+public abstract class Shooter : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public abstract void Start();
+    public abstract void Update();
+    public abstract void Shoot(GameObject shellPrefab, float fireforce, float damageDone, float lifespan);
 }
